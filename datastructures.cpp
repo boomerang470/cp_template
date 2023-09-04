b9a2e1943341338987b89ca43a849a87799d12f9
boomerang470 <124457768+boomerang470@users.noreply.github.com>
2023-09-04 18:07:30 +0530

    Added fenwick tree

--- datastructures.cpp
+++ datastructures.cpp
@@ -52,3 +52,35 @@ struct DSU{
         }
     }
 };
+
+
+
+
+//Fenwick Tree
+struct FenwickTree{
+    vector<int> bit;
+    int n;
+
+    FenwickTree(int n){
+        this->n = n;
+        bit.assign(n,0);
+    }
+
+    int sum(int r){
+        int ret=0;
+        for(;r>=0;r=(r&(r+1))-1){
+            ret += bit[r];
+        }
+        return ret;
+    }
+
+    int sum(int l,int r){
+        return sum(r)-sum(l-1);
+    }
+
+    void add(int idx,int delta){
+        for(;idx<n;idx=idx|(idx+1)){
+            bit[idx]+=delta;
+        }
+    }
+};
